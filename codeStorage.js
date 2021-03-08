46eac88c0aca37ff4d30fb930d192503e0566c6b
HawraAmeer <hwra.ameer97@gmail.com>
2021-03-08 12:07:51 +0300

    navigation component added

---
+++ codeStorage.js
@@ -0,0 +1,17 @@
+// App.js
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+<View style={styles.container}>
+  <Text>Open up App.js to start working on your app!</Text>
+  <StatusBar style="auto" />
+</View>;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
